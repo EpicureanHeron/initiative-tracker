0494894ddb74d430da3918b21c19d61f8f104944
Joe Thor <joethor1989@gmail.com>
2018-06-02 08:56:05 -0500

    new enemy class (?) button created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,14 @@
+//TODO
+
+//Tried to sort the combantantArr by init like I do with the PlayerArr, but for whatever reason the sorting is not working, it seems randomly out of order.
+
+//Need to generate a new <div> with its own placement when an enemey is created
+
+//Not sure if I can create multiple enemies with the "constructor" I am using
+
+
+
+
 var dinty = {
     name: "Dinty",
     playerID: "#dinty",
@@ -30,7 +41,11 @@ var sheldon = {
 
 var playerArr = [dinty, dymby, tarkus, magthar, sheldon]
 
+var combantantArr = [];
 
+var enemyCounter = 0
+
+var sortPressed = false;
 
 //initiliazes the power of jQuery
 $(document).ready(function() {
@@ -44,12 +59,15 @@ $(document).ready(function() {
        var NewInit = prompt("What is the characters init?");
 
        playerArr[index].init = NewInit
+
+       combantantArr.push(playerArr[index])
+
        console.log(playerArr)
        documentWrite()
     })
     //clicking the sort button sorts the PCs by init and then generates that order on screen
     $("#sort").click(function() {
-        
+        sortPressed = true;
         sortByInit();
         console.log(playerArr)
 
@@ -58,16 +76,43 @@ $(document).ready(function() {
 
 
     })
+    $("#newEnemy").click(function(){
+        
+        //prompts user for name of enemy and saves it 
+        var newname=  prompt("name for enemy")
+       //prompts user for the initiative of enemy
+        var init = prompt("init?")
+        //constructs a new enemy based on the class Enemy
 
+
+        var Enemy = new NewEnemy(newname, init);
+
+        combantantArr.push(Enemy)
+
+        console.log(Enemy)
+
+        console.log(combantantArr)
+
+
+    })
    
 });
 
 function documentWrite() {
-
-    for (i = 0; i < playerArr.length; i++) {
-        $(playerArr[i].playerID).html("<p>"+ playerArr[i].name + "<br>" +"init: " + playerArr[i].init + "</p>");
+  
+        for (i = 0; i < playerArr.length; i++) {
+            $(playerArr[i].playerID).html("<p>"+ playerArr[i].name + "<br>" +"init: " + playerArr[i].init + "</p>");
     }
+   
 }
+//this is a class
+function NewEnemy(name, init) {
+    this.name = name;
+    this.init = init;
+}
+
+
+
 
 function sortByInit() {
     console.log(playerArr)
@@ -86,7 +131,10 @@ function killCharacter(character) {
 }
 
 //Create an enenmy NPC
-function newEnemy() {
+function newEnemy(name, init) {
+
+
+
 
 }
 //orders the figherArr based on init 
